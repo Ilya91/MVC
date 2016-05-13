12bdcfead30c31eff4c8c331bf0f87ca3d0ac5b6
Mironchikov Ilya <mir19on@mail.ru>
2016-05-13 11:31:12 +0300

    +add() +getRoutes()

--- Core/Router.php
+++ Core/Router.php
@@ -2,5 +2,21 @@
 
 class Router
 {
+    /**
+     * @var
+     */
+    protected $routes;
 
+    /**
+     * @param $route
+     * @param $params
+     * @return void
+     */
+    public function add($route, $params){
+        $this->routes[$route] = $params;
+    }
+
+    public function getRoutes(){
+        return $this->routes;
+    }
 }
